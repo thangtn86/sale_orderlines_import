c9ab7247daecc45656e603eb26c09b383217da53
Thang Tong <nthang.tong@gmail.com>
2023-09-09 11:05:38 +0700

    Init repo

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+#-*- coding: utf-8 -*-
+{
+    'name': 'Sale Orderlines Import',
+    'version': '16.0.1.0.1',
+    'license': 'AGPL-3',
+    'author': 'Thang Tong <nthang.tong@gmail.com>',
+    'category': 'Sales Management',
+    'summary': 'Adding button to import order lines on Quotation and Sale Orders',
+    'description': '',
+    'website': '',
+    'depends': ['sale'],
+    'data': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
